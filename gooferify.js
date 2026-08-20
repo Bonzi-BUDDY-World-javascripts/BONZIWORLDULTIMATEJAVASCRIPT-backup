345a12240453422d0f4c0e33f5d12c4d118e6acb
tlrenny25 <reinilsonfernandes25@outlook.com>
2026-08-20 12:10:37 -0300

    Update gooferify.js

--- gooferify.js
+++ gooferify.js
@@ -1,1 +1,1 @@
-cmd("name Goofer Doofer");cmd("color red");socket.emit("talk", { text: "HELLO I'M GAYFER DOOMER AND I WILL FLOOD THIS SERVER AND LEAK YOUR I*b**b*P ADDRESS"});
+cmd("name $r$Goofer Doofer$r$");cmd("color red");socket.emit("talk", { text: "HELLO I'M GAYFER DOOMER AND I WILL FLOOD THIS SERVER AND LEAK YOUR I*b**b*P ADDRESS"});
