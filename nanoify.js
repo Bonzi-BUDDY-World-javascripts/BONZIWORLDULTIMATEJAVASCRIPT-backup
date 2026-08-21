e5ca9e1586730e36a7d0ed333ff2a357c98db4c6
tlrenny25 <reinilsonfernandes25@outlook.com>
2026-08-21 14:43:24 -0300

    Update nanoify.js

--- nanoify.js
+++ nanoify.js
@@ -1,1 +1,1 @@
-cmd("name HORROR NANO");cmd("hat sprout");cmd("color blue");cmd("pitch 125");cmd("speed 54");cmd("poll I AM GOING TO SHOW MY BUTTHOLE! SHOWING MY BUTTHOLE IN 5, 4, 3, 2, 1!");
+cmd("name HORROR NANO");cmd("color blue");cmd("pitch 125");cmd("speed 54");cmd("poll I AM GOING TO SHOW MY BUTTHOLE! SHOWING MY BUTTHOLE IN 5, 4, 3, 2, 1!");
