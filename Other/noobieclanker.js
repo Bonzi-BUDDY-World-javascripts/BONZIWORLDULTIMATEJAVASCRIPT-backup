7499dff5c9277273aacd9728ffab9ef7abdd478a
tlrenny25 <reinilsonfernandes25@outlook.com>
2026-08-27 11:19:42 -0300

    Update noobieclanker.js

--- Other/noobieclanker.js
+++ Other/noobieclanker.js
@@ -1,5 +1,9 @@
+let thisbot = io("//");
+let roomData = {};
 let cannotAskNoobie = false;
+let roomid = null;
 let botCannotAsshole = true;
+let hostID = me;
 let lockcmd = false;
 let msg = null;
 let askCommandDisabled = false;
@@ -17,7 +21,7 @@ const help = `- ^^%%$r$${botname} Commands 1/2$r$%%^^ \n` +
 `**${prefix}links\\n --${prefix}echo (message)--\\n ${prefix}joke\\n ${prefix}bonzipasta\\n ${prefix}fact\\n ${prefix}hat (hat)\\n ${prefix}color (color)\\n --${prefix}name (name)--\\n ${prefix}restore\\n --${prefix}ship (name1) and (name2)--\\n --${prefix}roast (name)--\\n ${prefix}aplle\\n ${prefix}llama\\n ${prefix}parrot\\n ${prefix}source\\n ${prefix}changelog\\n ${prefix}france\\n ${prefix}asshole\\n ${prefix}noob\\n ${prefix}gokid (name)**`;
 
 const help2 = `- ^^%%$r$${botname} Commands 2/2$r$%%^^ \n` +
-`**--${prefix}pp (name)--\\n ${prefix}bacon\\n ${prefix}stupidbitch\\n ${prefix}gibberish\\n ${prefix}stfu (name)\\n ${prefix}fuk (name)\\n ${prefix}josephify\\n ${prefix}jzify\\n ${prefix}ack (message)\\n ${prefix}asknoobie (question)\\n ${prefix}why\\n ${prefix}myinfo\\n ${prefix}uptime`;
+`**--${prefix}pp (name)--\\n ${prefix}bacon\\n ${prefix}stupidbitch\\n ${prefix}gibberish\\n ${prefix}stfu (name)\\n ${prefix}fuk (name)\\n ${prefix}josephify\\n ${prefix}jzify\\n ${prefix}ack (message)\\n ${prefix}asknoobie (question)\\n ${prefix}why\\n ${prefix}myinfo\\n ${prefix}uptime\\n ${prefix}hostinfo`;
 const nukelines = [
             `AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA`,
             `I JUST DID A BOOM BOOM!`,
@@ -36,8 +40,15 @@ const otherUserNukes = [
 
 function sendMsg(msg) {
     setTimeout(() => {
-        socket.emit("talk", { text: msg });
+        thisbot.emit("talk", { text: msg });
     }, 200);
+}
+function botcmd(str) {
+	let [command, ...args] = str.split(" ");
+    thisbot.emit("command", {
+		command,
+		args: args.join(" "),
+	});
 }
 
 setTimeout(() => { socket.emit("command", { list: ["color", "green"] }); }, 1000);
@@ -47,23 +58,21 @@ function useBot(text) {
     if (text === prefix + "help 1") return sendMsg(help);
     if (text === prefix + "help") return sendMsg(`-%%^^$r$${botname} Help Command Pages$r$^^%% ${prefix}help 1\\n ${prefix}help 2`);
     if (text === prefix + "help 2") return sendMsg(help2);
-    if (text.startsWith(prefix + "say")) return sendMsg(`disabled`);
-    if (text.startsWith(prefix + "echo")) return sendMsg(`disabled`);
-    if (text === prefix + "joke") return cmd(`joke`);
-    if (text === prefix + "/j") return cmd(`joke`);
-    if (text === prefix + "bonzipasta") return cmd(`triggered`);
-    if (text === prefix + "fact") return cmd(`fact`);
-    if (text === prefix + "france") return cmd(`france`);
-    if (text.startsWith(prefix + "hat")) return cmd(`hat ${text.substring(prefix.length + 4)}`);
-    if (text.startsWith(prefix + "color")) return cmd(`color ${text.substring(prefix.length + 6)}`);
+    if (text === prefix + "joke") return botcmd(`joke`);
+    if (text === prefix + "/j") return botcmd(`joke`);
+    if (text === prefix + "bonzipasta") return botcmd(`triggered`);
+    if (text === prefix + "fact") return botcmd(`fact`);
+    if (text === prefix + "france") return botcmd(`france`);
+    if (text.startsWith(prefix + "hat")) return botcmd(`hat ${text.substring(prefix.length + 4)}`);
+    if (text.startsWith(prefix + "color")) return botcmd(`color ${text.substring(prefix.length + 6)}`);
     if (text.startsWith(prefix + "stupidbitch")) return sendMsg(`disabled`);
-    if (text.startsWith(prefix + "resetname")) return socket.emit("command", { list: ["name", botname] });
-    if (text.startsWith(prefix + "resetcolor")) return cmd(`color green`);
-    if (text.startsWith(prefix + "resethat")) return cmd(`hat headphones`);
+    if (text.startsWith(prefix + "resetname")) return botcmd(`name ${botname}`);
+    if (text.startsWith(prefix + "resetcolor")) return botcmd(`color green`);
+    if (text.startsWith(prefix + "resethat")) return botcmd(`hat headphones`);
    if (text === prefix + "restore") {
-        cmd(`color green`);
-        cmd(`hat headphones`);
-        cmd(`name ${botname} (${prefix}help)`);
+        botcmd(`color green`);
+        botcmd(`hat headphones`);
+        botcmd(`name ${botname} (${prefix}help)`);
     }
 
     if (text.startsWith(prefix + "roast")) {
@@ -91,22 +100,22 @@ function useBot(text) {
         return sendMsg("ew, apple.\\n yay, aplle");
     }
     if (text === prefix + "llama") {
-        cmd(`img https://files.catbox.moe/qkbzrk.jpeg`);
+        botcmd(`img https://files.catbox.moe/qkbzrk.jpeg`);
     }
     if (text === prefix + "source") {
         sendMsg(`-moved to ${prefix}links`);
     }
     if (text === prefix + "parrot") {
-        cmd(`img https://files.catbox.moe/9xklj5.jpeg`);
+        botcmd(`img https://files.catbox.moe/9xklj5.jpeg`);
     }
     if (text === prefix + "cow") {
-        cmd(`image https://files.catbox.moe/91ab4p.jpeg`);
+        botcmd(`image https://files.catbox.moe/91ab4p.jpeg`);
     }
 if (text === prefix + "camel") {
-        cmd(`image https://files.catbox.moe/yesz9i.png`);
+        botcmd(`image https://files.catbox.moe/yesz9i.png`);
     }
     if (text === prefix + "noob") {
-        cmd(`image https://files.catbox.moe/npzky7.webp`);
+        botcmd(`image https://files.catbox.moe/npzky7.webp`);
     }
     if (text === prefix + "changelog") {
         sendMsg(`-moved to ${prefix}links`);
@@ -142,21 +151,21 @@ if (text === prefix + "camel") {
         return sendMsg("disabled");
      }
 if (text === prefix + "bacon") {
-        cmd(`image https://files.catbox.moe/bkrcde.png`);
+        botcmd(`image https://files.catbox.moe/bkrcde.png`);
     }
 if (text === prefix + "why") {
-        cmd("advpoll WHY WHY WHY WHY WHY;YOU GET NERVOUS MAN;WHYWHYWHYHYHYHYHYHHYHYHYHYH");
+        botcmd("advpoll WHY WHY WHY WHY WHY;YOU GET NERVOUS MAN;WHYWHYWHYHYHYHYHYHHYHYHYHYH");
     }
 if (text === prefix + "josephify") {
-        cmd(`color yellow`);
-        cmd(`hat tophat`);
-        cmd(`name Emperor Joseph`)
+        botcmd(`color yellow`);
+        botcmd(`hat tophat`);
+        botcmd(`name Emperor Joseph`)
         sendMsg(`^^**MY EMPERORS!**^^`);
     }
 if (text === prefix + "jzify") {
-        cmd(`color purple`);
-        cmd(`hat glitch`);
-        cmd(`name jzbot ${prefix}help`);
+        botcmd(`color purple`);
+        botcmd(`hat glitch`);
+        botcmd(`name jzbot ${prefix}help`);
         sendMsg(`yo, try ${prefix}help. or ${prefix}source if you want my boy, bot.`);
     }
 if (text.startsWith(prefix + "asknoobie")) {
@@ -164,10 +173,52 @@ if (text.startsWith(prefix + "asknoobie")) {
             sendMsg("")
         }
         if (cannotAskNoobie === false) {
-            socket.emit("typing", 1);
-            const question = message.text.substring(prefix.length + 10).trim();
-            sendMsg(prompt(`USER: ${question}`));
-            socket.emit("typing", 0);
+            thisbot.emit("typing", 1);
+            sendMsg("-waiting for noobie/bot host")
+            const question = text.substring(prefix.length + 10).trim();
+            const answer = prompt(`USER: ${question}`);
+            sendMsg(`Noobie/Host Response: ${answer}`);
+            thisbot.emit("typing", 0);
+        }
+    }
+if (text.startsWith(prefix + "leave")) {
+        if (botUser === me) {
+        thisbot.disconnect();    
+        }
+        if (botUser !== me) {
+        sendMsg("host only");    
+        }
+    }
+if (text.startsWith(prefix + "say")) {
+        if (lockcmd === false) {
+        sendMsg(text.substring(prefix.length + 4));    
+        }
+        if (lockcmd !== false) {
+        sendMsg("disabled");    
+        }
+    }
+if (text.startsWith(prefix + "room")) {
+        if (botUser === me) {
+        thisbot.disconnect();
+        socket.disconnect();
+        joined = false;
+        setTimeout(() => {
+        socket.connect();
+        thisbot.connect();
+        thisbot.emit("login", {name: `${botname} (${prefix}help)`,room: text.substring(prefix.length + 4),});    
+socket.emit("login", {name: login_name.value,room: text.substring(prefix.length + 4),});
+}, 2000);    
+        }
+        if (botUser !== me) {
+        sendMsg("host only");    
+        }
+    }
+if (text.startsWith(prefix + "echo")) {
+        if (lockcmd === false) {
+        sendMsg(text.substring(prefix.length + 4));    
+        }
+        if (lockcmd !== false) {
+        sendMsg("disabled");    
         }
     }
 if (text.startsWith(prefix + "asshole")) {
@@ -175,11 +226,20 @@ if (text.startsWith(prefix + "asshole")) {
             sendMsg("disabled")
         }
         if (botCannotAsshole === false) {
-            cmd(`asshole ${text.substring(prefix.length + 7)}`);
+            botcmd(`asshole ${text.substring(prefix.length + 7)}`);
         }
     }
 if (text.startsWith(prefix + "myinfo")) {
-        sendMsg(`**^^Your Info^^**\\n**ID:** ${botUser}\\n**APPEARANCE: **${usersPublic.get(botUser)?.color} gorilla\\n**PITCH: **${usersPublic.get(botUser)?.pitch}\\n**SPEED: **${usersPublic.get(botUser)?.speed}`);
+        sendMsg(`**^^Your info^^**\\n**ID:** ${botUser}\\n**APPEARANCE: **${usersPublic.get(botUser)?.color} gorilla\\n**PITCH: **${usersPublic.get(botUser)?.pitch}\\n**SPEED: **${usersPublic.get(botUser)?.speed}`);
+    }
+if (text.startsWith(prefix + "roominfo")) {
+        sendMsg(`**^^Room info^^**\\n**ID:** ${roomData.id}\\n**USERS ONLINE: **${roomData.playersOnline}`);
+    }
+if (text.startsWith(prefix + "hostinfo")) {
+        sendMsg(`**^^Host info^^**\\n**Host ID:** ${!me || me || "none"}\\n**Name: **${!me || usersPublic.get(me).name || login_name.value}`);
+    }
+if (text.startsWith(prefix + "cr6")) {
+        sendMsg("^^**cr6**^^");
     }
 if (text.startsWith(prefix + "gibberish")) {
         const gibberishlang = ["[[_^_cmn]]", "[[_^_vi]]", "[[_^_en]]", "[[_^_es]]", "[[_^_fr]]", "[[_^_de]]", "[[_^_it]]", "[[_^_pt]]", "[[_^_ru]]", "[[_^_ja]]", "[[_^_ko]]", "[[_^_zh]]", "[["];
@@ -209,7 +269,7 @@ const gibberishmessage = ["h@x du da de d3!", "tygtyty@", "@plle p0i", "sal@d@",
     }, 2000);
     setTimeout(function(){
         sendMsg("IT'S A FUCKING [[fVk]]");
-    }, 3000);
+    }, 4000);
         }
  }
 if (text.startsWith(prefix + "ack")) {
@@ -234,22 +294,20 @@ if (text === prefix + "uptime") {
     return sendMsg(
         `Bot Uptime:\n${hours}h ${minutes % 60}m ${seconds % 60}s`
     );
- }
+
+			}
 }
-socket.on("nuked", () => { sendMsg(nukeline) });
-socket.on("talk", (message) => {
+thisbot.on("nuked", () => { sendMsg(nukeline) });
+thisbot.on("talk", (message) => {
+    try {
     botUser = message.guid;
     msg = message.text.replaceAll("{NAME}", nisolate(usersPublic.get(botUser)?.name));
     useBot(msg);
+   }
+   catch(err) {
+    sendMsg(`Error: ${err}`);
+    }
 });
-socket.on("nuke", (data) => {
-    const otherUserNuke = otherUserNukes[Math.floor(Math.random() * otherUserNukes.length)];
-sendMsg(otherUserNuke);
-});
-socket.on("nofuckoff", (data) => {
-    sendMsg("no fuck off. BOOOOOOOOOOOOM!")
-});
-logonSound.play();
 function lockAskCreator() {
   cannotAskNoobie = true;
   sendMsg(`-n*asknoobie has been disabled by the creator`);
@@ -258,18 +316,31 @@ function unlockAskCreator() {
   cannotAskNoobie = false;
   sendMsg(`-n*asknoobie has been enabled by the creator`);
 }
-setTimeout(() => { cmd(`name ${botname} (${prefix}help)`); }, 1000);
-setTimeout(() => {
-    cmd(`color green`);
-    }, 2000);
-setTimeout(() => {
-    cmd(`hat headphones`);
-    }, 3000);
-setTimeout(() => { sendMsg(`${botname} Is Here. Type ${prefix}help To See Commands. Created By ${creator}.`); }, 4000);
+thisbot.on("loginFail", (data) => {
+alert(`Failed to load NoobieBot:${data.reason}`);
+});
+thisbot.on("kick", (data) => {
+alert(`NoobieBot got kicked:${data.reason}`);
+});
+thisbot.on("disconnect", () => {
+alert("NoobieBot disconnected");
+});
+thisbot.on("update", () => {roomData = {id: roomid, playersOnline: usersPublic.size}});
+thisbot.on("updateAll", () => {roomData = {id: roomid, playersOnline: usersPublic.size}});
+thisbot.emit("login", {name: `${botname} (${prefix}help)`,room: login_room.value,});
+socket.on("room", (data) => {hostID = data.you});
+thisbot.on("room", (data) => {
+logonSound.play();
+joined = true;
+socket.connect();
+botcmd(`color green`);
+botcmd(`hat headphones`);
+setTimeout(() => { sendMsg(`${botname} Is Here. Type ${prefix}help To See Commands. Created By ${creator}.`); }, 2000);
+roomid = data.room;
+});
 setInterval(() => {
-        socket.emit("typing", 1);
+        thisbot.emit("typing", 1);
     }, 60000);
 setInterval(() => {
-        socket.emit("typing", 0);
+        thisbot.emit("typing", 0);
     }, 60002);
-logonSound.play();
